53afbb38b05949e384b1765e4645edca2e7b9341
LizzieK549 <klizzy630@gmail.com>
2024-10-11 17:26:34 -0500

    Week 6 program
    Lizzie Kerr, 10/11/2024, Week 6 assignment

--- program_1.py
+++ program_1.py
@@ -2,14 +2,30 @@
 # Program #1: Random Dice
 # Write a "randDice" function (with no input) that randomly chooses two numbers between 1 and 6 (inclusive) and then adds them (this is to simulate the rolling of 2 dice).  
 # The dice sum will be the output of this function.
+import random
 
 def randDice():
     # Write your logic to generate 2 numbers between 1 and 6 here
-
+    roll1 = random.randint(1, 6)
+    roll2 = random.randint(1, 6)
     # Sum 2 numbers
-
+    sum2 = roll1 + roll2
     # return sum to calling function
+    return sum2
 
 #########
 # Then write a mainline that calls the "randDice" function 100 times in a for loop.  
 # The mainline then prints the average of the 100 rolls, rounded to the nearest 0.01.
+def main():
+    total = 0
+    rolls = 100
+
+    for n in range(rolls):
+        total += randDice()
+
+    average = total/rolls
+    print("Average of 100 rolls is "f"{average:,.2f}")
+
+main()
+
+
--- program_3.py
+++ program_3.py
@@ -9,3 +9,28 @@
 # The amount of state sales tax.
 # The total sales tax (county plus state)
 # Use at least one function with input and output in this program
+
+StateTax = .05
+CountyTax = .025
+
+def main():
+    TotalSales = float(input("Enter the total sales for the month: "))
+
+
+    #Get/call tax amounts
+    state_tax = statetaxmethod(TotalSales)
+    county_tax = countytaxmethod(TotalSales)
+
+    #Calculate total
+    total = state_tax + county_tax
+
+    print("State Tax = "f"${state_tax:,.2f}")
+    print("County Tax = "f"${county_tax:,.2f}")
+    print("Total Tax = "f"${total:,.2f}")
+
+def statetaxmethod(TotalSales):
+    return TotalSales * StateTax
+def countytaxmethod(TotalSales):
+    return TotalSales * CountyTax
+
+main()
